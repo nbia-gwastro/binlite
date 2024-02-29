cc7118f7534aad8006206bd7027ecd9ec8da6cc0
Chris <22056557+ctiede@users.noreply.github.com>
2024-02-29 11:39:59 +0100

    Update

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup, find_packages
 
-MAJOR   = 0
-MINOR   = 1
-VERSION = f"{MAJOR}.{MINOR}"
-# MICRO = 1
-# VERSION = f"{MAJOR}.{MINOR}.{MICRO}"
+MAJOR = 0
+MINOR = 1
+MICRO = 0
+VERSION = f"{MAJOR}.{MINOR}.{MICRO}"
 
 NAME             = "binlite"
-AUTHOR           = "Daniel J. D'Orazio"
-AUTHOR_EMAIL     = "daniel.dorazio@nbi.ku.dk"
-DESCRIPTION      = 'Rapidly generate periodic lightcurves of accreting binaries'
-LONG_DESCRIPTION = 'Generate photometric variability templates for equal mass \
+AUTHOR           = "Christopher Tiede & Daniel D'Orazio"
+DESCRIPTION      = "Rapidly generate periodic lightcurves of accreting binaries"
+LONG_DESCRIPTION = "Generate photometric variability templates for equal mass \
                     binaries in co-planar gaseous disks, spanning a continuous \
                     range of orbital eccentricities up to 0.8 for both prograde \
-                    and retrograde systems'
-# PYTHON_REQUIRES = ">=" + min_version
+                    and retrograde systems"
+GIT_URL = "https://github.com/nbia-gwastro/binlite"
 
 # =============================================================================
 def setup_package():
@@ -23,18 +21,15 @@ def setup_package():
         version = VERSION,
         author  = AUTHOR,
         maintainer = AUTHOR,
-        author_email = AUTHOR_EMAIL,
         description  = DESCRIPTION,
         long_description = LONG_DESCRIPTION,
         packages = find_packages(),
         include_package_data = True,
-        package_data = {"binlite": ["data/*.dat"]}, # if do this shouldn't need a MANIFEST.in
+        package_data = {'binlite': ['data/*.dat']},
         license = "MIT",
-        platforms = ["Any"],
-        # url = GIT_URL,
-        # download_url = DOWNLOAD_URL,
-        # install_requires = NONE,
-        # python_requires = PYTHON_REQUIRES,
+        platforms = ['Any'],
+        url = GIT_URL,
+        install_requires = ['numpy'],
     )
     setup(**metadata)
 
